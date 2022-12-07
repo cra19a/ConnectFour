70b67eec1c970a31c936be82e7e4a6259dba3298
Clayton Arnett <89483607+cra19a@users.noreply.github.com>
2022-12-07 12:54:56 -0600

    Add files via upload

---
+++ ConnectFourCRA/ConnectFourCRA/BoardCell.swift
@@ -0,0 +1,13 @@
+//
+//  BoardCell.swift
+//  ConnectFourCRA
+//
+//  Created by Clayton Arnett on 12/6/22.
+//
+
+import UIKit
+
+class BoardCell: UICollectionViewCell
+{
+    @IBOutlet weak var image: UIImageView!
+}
---
+++ ConnectFourCRA/ConnectFourCRA/BoardItem.swift
@@ -0,0 +1,51 @@
+//
+//  BoardItem.swift
+//  ConnectFourCRA
+//
+//  Created by Clayton Arnett on 12/6/22.
+//
+
+import Foundation
+import UIKit
+
+enum Tile
+{
+    case Red
+    case Yellow
+    case Empty
+}
+
+struct BoardItem
+{
+    var indexPath: IndexPath!
+    var tile: Tile!
+    
+    func yellowTile() -> Bool
+    {
+        return tile == Tile.Yellow
+    }
+    
+    func redTile() -> Bool
+    {
+        return tile == Tile.Red
+    }
+    
+    func emptyTile() -> Bool
+    {
+        return tile == Tile.Empty
+    }
+    
+    func tileColor() -> UIColor
+    {
+        if redTile()
+        {
+            return .red
+        }
+        if yellowTile()
+        {
+            return .systemYellow
+        }
+        
+        return .white
+    }
+}
